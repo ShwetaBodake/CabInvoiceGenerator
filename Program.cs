d0971159c14a8372f980fd767c50915cb80faf8e
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-11 19:23:00 +0530

    Calling Invoice generator class in main method

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace CabInvoiceGenerator
+{
+
+public class Program
+    {
+       
+            public enum Month
+        {
+            Jan,Feb,March,April,May
+        };
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the CabInvoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double Fare = invoiceGenerator.CalculateFare(2.0 ,5);
+            Console.WriteLine(Fare);
+        }
+    }
+}
